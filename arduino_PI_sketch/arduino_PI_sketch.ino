af6142e248070ba94dea103949ed8916f0be7864
apetsiuk <apetsiuk@mtu.edu>
2019-04-28 19:42:13 -0400

    added all files

--- arduino_PI_sketch/arduino_PI_sketch.ino
+++ arduino_PI_sketch/arduino_PI_sketch.ino
@@ -1,47 +1,5 @@
-
-# Arduino PI Motor Control
-
- >*PWM-based DC motor PI control implemented on Arduino UNO*
-<br/> 
-
-### Developing an Observer and a Controller
-
-Considering the following system, we will design an PI controller for the system. <br/> 
-
-![Experimental Model](images/Fig_1.JPG) <br/> 
-**Figure 1.** Experimental Model
-<br/> 
-
-To generate the desired output, we will use a Pulse Width Modulated signal with an amplitude of 10 volts. The duty cycle gives you an average amplitude from 0 to 5 volts. <br/> 
-
-![Duty Cycle Development](images/Fig_3.JPG) <br/> 
-**Figure 2.** Duty Cycle Development
-<br/> 
-
-The default PWM frequency is about 980 Hz, and the PWM frequency is controlled by three internal timers: timer0, timer1, and timer2. The instruction to change PWM frequency is `TCCRnB = (TCCRnB & 0xF8) | 2` where n is chosen depending on which timer is associated with the PWM pin whose frequency we want to change. <br/> 
-
-![Arduino Connections](images/Fig_3.JPG) <br/> 
-**Figure 3.** Arduino Connections
-<br/> 
-
-![Default PWM](images/Fig_5.JPG) <br/> 
-**Figure 4.** Default PWM Frequency ~ 970 Hz
-<br/> 
-
-The TCCR2B would not affect the system’s clock time (it affects only Timer2 rate), so the `millis()` function will give correct timings. The duty cycle with higher frequency will give us smooth velocity change and the motor will not provide high pitch sound. <br/>
-
-![TCCR2B PWM](images/Fig_6.JPG) <br/> 
-**Figure 5.** TCCR2B PWM Frequency ~ 3.9 kHz
-<br/> 
-
-The PID algorithm is shown in the following equation: <br/> 
-![PID Equation](images/Fig_4.JPG) <br/> <br/> 
-
-### Arduino Code
-
-```C
 /************************************
- *          Motor Speed Control     *
+ *          Motor Speed Control     *       
  ************************************/
 
 //PID constants
@@ -81,7 +39,7 @@ void setup(){
   // high sound
 
   Serial.begin(9600);
-} 
+}    
  
 void loop(){
 
@@ -149,20 +107,9 @@ float bound(float x, float x_min, float x_max){
   if(x > x_max){x = x_max;}
   return x;
 }
-```
-<br/> <br/>
 
-The `Serial.print()` function is time consuming operation which will increase the integration cycle by approximately 80 ms. <br /> 
+        
+ 
 
-![Serial Monitor Output](images/serial.PNG) <br/> 
-**Figure 6.** Serial Monitor Output
-<br/>
-
-
-![Transient test](images/Fig_7.JPG) <br/> 
-**Figure 7.** Transient test
-<br/>  <br/> 
-
--- end --
 
 
